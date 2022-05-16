937c10608ae158ecfc26fca74571da29b63635bb
shit-ware <frontlinemods@gmail.com>
2022-05-15 23:49:41 -0500

    add cmd functions

--- src/game/Cmd/Cmd.hpp
+++ src/game/Cmd/Cmd.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
 #include "../stdafx.hpp"
+#include "../defs.hpp"
 
-int Cmd_AddCommandInternal(const char* s1, void(*callback)(), void* allocation, bool key);
+cmd_function_s* Cmd_FindCommand(const char* cmdName);
+void Cmd_AddCommandInternal(const char* cmdName, void(__cdecl* function)(), cmd_function_s* allocedCmd, std::int32_t flags);

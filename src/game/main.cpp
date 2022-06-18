fba8c086e883e64b7b9dd0841d8939896313e899
shit-ware <10711154+shit-ware@users.noreply.github.com>
2022-06-17 21:04:01 -0500

    remove zynamicpatch for now

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -136,8 +136,3 @@ int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdL
         return 0;
     }
 }
-
-void ZynamicPatch()
-{
-    memory::replace(0x06BABA1, WinMain);
-}

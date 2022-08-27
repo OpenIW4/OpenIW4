66bcad8a635f04f61c996de74b337b820d8fc9c7
shit-ware <frontlinemods@gmail.com>
2022-08-27 12:27:23 -0500

    rename param

--- src/game/Dvar/Dvar.cpp
+++ src/game/Dvar/Dvar.cpp
@@ -9,10 +9,10 @@ void Dvar_Init()
 }
 
 //THUNK : 0x00648440
-int Dvar_RegisterFloat(char* command_name, float default_value, float minimum_value, float maximum_value, int a5, char* command_description)
+int Dvar_RegisterFloat(char* command_name, float default_value, float minimum_value, float maximum_value, int flags, char* command_description)
 {
 	return memory::call<int(char*, float, float, float, int, char*)>(0x00648440)
-		(command_name, default_value, minimum_value, maximum_value, a5, command_description);
+		(command_name, default_value, minimum_value, maximum_value, flags, command_description);
 }
 
 //THUNK : 0x004CE1A0
--- src/game/Dvar/Dvar.hpp
+++ src/game/Dvar/Dvar.hpp
@@ -3,5 +3,5 @@
 #include "../stdafx.hpp"
 
 void Dvar_Init();
-int Dvar_RegisterFloat(char* command_name, float default_value, float minimum_value, float maximum_value, int a5, char* command_description);
+int Dvar_RegisterFloat(char* command_name, float default_value, float minimum_value, float maximum_value, int flags, char* command_description);
 int Dvar_RegisterBool(const char* a1, bool a2, unsigned int a3, const char* a4);
